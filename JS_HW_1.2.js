c48ca648ae7303188aa0ad9151c3cac513a7633d
mstepanova1 <101565144+mstepanova1@users.noreply.github.com>
2022-04-26 00:15:02 +0300

    Update JS_HW_1.2.js

--- JS_HW_1.2.js
+++ JS_HW_1.2.js
@@ -3,7 +3,7 @@ let age_3 = 60;
 
 const checkAge = function(age) {
     console.log("If your age = " + age)
-    if (Number(age)) {
+    if (Number(age) || age == 0) {
         if (age < age_2) {
             console.log("You don't have access cause your age is " + age + ". It's less then " + age_2)
         } else if (age >= age_2 && age < age_3) {
@@ -18,6 +18,7 @@ const checkAge = function(age) {
     }    
 } 
 
+checkAge(0);
 checkAge(17);
 checkAge(18);
 checkAge(61);
