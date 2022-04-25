e50d4c908e325ede5e6e82185ccb20b5b3a7d14d
mstepanova1 <101565144+mstepanova1@users.noreply.github.com>
2022-04-25 22:39:19 +0300

    Add files via upload

---
+++ JS_HW_1.2.js
@@ -0,0 +1,29 @@
+let age_2 = 18;
+let age_3 = 60;
+
+const checkAge = function(age) {
+    console.log("If your age = " + age)
+    if (Number(age)) {
+        if (age < age_2) {
+            console.log("You don't have access cause your age is " + age + ". It's less then " + age_2)
+        } else if (age >= age_2 && age < age_3) {
+            console.log("Welcome!")
+        } else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        } else {
+            console.log("Technical work")
+        }
+    } else {
+        console.log("Invalid value")
+    }    
+} 
+
+checkAge(17);
+checkAge(18);
+checkAge(61);
+checkAge('qwerty');
+checkAge('25');
+checkAge('25ffff');
+
+// let agePrompt = prompt('Enter age')
+// checkAge(agePrompt)
